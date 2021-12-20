0cb4a7bc262962bbea11098256620e8d6f2f92a0
devjony <jonatamicael@gmail.com>
2021-12-20 17:00:31 -0300

    remove unused annotation

--- src/main/java/com/devjony/rabbitmqdemo/RabbitmqDemoApplication.java
+++ src/main/java/com/devjony/rabbitmqdemo/RabbitmqDemoApplication.java
@@ -1,11 +1,9 @@
 package com.devjony.rabbitmqdemo;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRabbit
 public class RabbitmqDemoApplication {
 
     public static void main(String[] args) {
