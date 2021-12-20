0e6e6a6c5a21749231f8fe9a2d9adcb705e63791
devjony <jonatamicael@gmail.com>
2021-12-20 17:01:31 -0300

    change rabbitTemplate for AmqpTemplate

--- src/main/java/com/devjony/rabbitmqdemo/rabbitmq/ProducerApi.java
+++ src/main/java/com/devjony/rabbitmqdemo/rabbitmq/ProducerApi.java
@@ -2,17 +2,17 @@ package com.devjony.rabbitmqdemo.rabbitmq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProducerApi {
 
-    private static final Logger logger = LoggerFactory.getLogger(ProducerApi.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(ProducerApi.class);
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private AmqpTemplate rabbitTemplate;
 
     public void send(String message) {
 
