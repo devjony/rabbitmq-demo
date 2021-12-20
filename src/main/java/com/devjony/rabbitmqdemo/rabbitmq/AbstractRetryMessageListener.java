7482df4102228f57f8d480e6f501fcf286ca3420
devjony <jonatamicael@gmail.com>
2021-12-20 17:02:21 -0300

    implement retries flow

--- src/main/java/com/devjony/rabbitmqdemo/rabbitmq/AbstractRetryMessageListener.java
+++ src/main/java/com/devjony/rabbitmqdemo/rabbitmq/AbstractRetryMessageListener.java
@@ -9,11 +9,8 @@ public abstract class AbstractRetryMessageListener implements MessageListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRetryMessageListener.class);
 
-    private static final Integer MAX_DELIVERIES = 10;
-
     @Override
     public final void onMessage(Message message) {
-//        MessageDecorator messageDecorator = new MessageDecorator(message);
         Integer retryHeader = message.getMessageProperties().getHeader("x-retry");
         if (retryHeader == null) {
             retryHeader = 1;
@@ -21,14 +18,16 @@ public abstract class AbstractRetryMessageListener implements MessageListener {
             retryHeader = Integer.valueOf(retryHeader) + 1;
         }
 
-        LOGGER.info("Message incoming... retry={}", retryHeader);
-
+        if(retryHeader <= 3)
+            LOGGER.info("Message incoming... retry={}", retryHeader);
 
         String expiration = String.valueOf(retryHeader * 5000);
         message.getMessageProperties().setExpiration(expiration);
         message.getMessageProperties().setHeader("x-retry", retryHeader);
 
-        LOGGER.info("Expiration: {}", expiration);
+        if(retryHeader <= 3)
+            LOGGER.info("Expiration: {}", expiration);
+
         doOnMessage(message);
     }
 
--- src/main/java/com/devjony/rabbitmqdemo/rabbitmq/Consumer.java
+++ src/main/java/com/devjony/rabbitmqdemo/rabbitmq/Consumer.java
@@ -1,30 +1,28 @@
 package com.devjony.rabbitmqdemo.rabbitmq;
 
-import com.rabbitmq.client.impl.AMQImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.core.AmqpNackReceivedException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Consumer extends AbstractRetryMessageListener {
 
     private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    RabbitTemplate rabbitTemplate;
 
     @Override
     protected void doOnMessage(Message message) {
-        logger.info("Body: " + new String(message.getBody()));
-        try {
-            throw new Exception("Teste retry");
-        } catch (Exception e) {
-//            rabbitTemplate.send("moip","order_app.payment.analysis.parking", message);
+        Integer retries = message.getMessageProperties().getHeader("x-retry");
+
+        if(retries <= 3) {
+            rabbitTemplate.convertAndSend("moip","order_app.payment.analysis.parking", message);
+        } else {
+            logger.info("max retries reached");
+            message.getMessageProperties().setExpiration(null);
+            rabbitTemplate.convertAndSend("moip", "order_app.payment.analysis.dlq", message);
         }
     }
 }
