2401645988321cfd91fd850185c09933f7440c50
devjony <jonatamicael@gmail.com>
2021-12-24 06:30:25 -0300

    use @value to queue's name

--- src/main/java/com/devjony/rabbitmqdemo/rabbitmq/Consumer.java
+++ src/main/java/com/devjony/rabbitmqdemo/rabbitmq/Consumer.java
@@ -5,10 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Consumer extends AbstractRetryMessageListener {
 
     private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    @Value("${spring.rabbitmq.exchange.name}")
+    String exchangeName;
+
+    @Value("${spring.rabbitmq.parking.queue.name}")
+    String parkingQueueName;
+
+    @Value("${spring.rabbitmq.failed.queue.name}")
+    String failedQueueName;
 
     @Autowired
     RabbitTemplate rabbitTemplate;
@@ -17,12 +27,12 @@ public class Consumer extends AbstractRetryMessageListener {
     protected void doOnMessage(Message message) {
         Integer retries = message.getMessageProperties().getHeader("x-retry");
 
-        if(retries <= 3) {
-            rabbitTemplate.convertAndSend("moip","order_app.payment.analysis.parking", message);
+        if(retries < 5) {
+            rabbitTemplate.convertAndSend(exchangeName, parkingQueueName, message);
         } else {
             logger.info("max retries reached");
             message.getMessageProperties().setExpiration(null);
-            rabbitTemplate.convertAndSend("moip", "order_app.payment.analysis.dlq", message);
+            rabbitTemplate.convertAndSend(exchangeName, failedQueueName, message);
         }
     }
 }
--- src/main/java/com/devjony/rabbitmqdemo/rabbitmq/ProducerApi.java
+++ src/main/java/com/devjony/rabbitmqdemo/rabbitmq/ProducerApi.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +12,19 @@ public class ProducerApi {
 
     private static final Logger logger = LoggerFactory.getLogger(ProducerApi.class);
 
+    @Value("${spring.rabbitmq.exchange.name}")
+    String exchangeName;
+
+    @Value("${spring.rabbitmq.queue.name}")
+    String queueName;
+
     @Autowired
     private AmqpTemplate rabbitTemplate;
 
     public void send(String message) {
 
         try {
-            rabbitTemplate.convertAndSend("moip","order_app.payment.analysis", message);
+            rabbitTemplate.convertAndSend(exchangeName, queueName, message);
         } catch (Exception e) {
             logger.error("Error sending message: " + e.getMessage());
         }
